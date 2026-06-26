7e00e789e89eacfe81d68dc5dbeded4d7a35b0fc
Rodeffs <guideandplaygamespilex24@gmail.com>
2026-06-26 11:40:12 +0200

    doesnt work somehow

--- main.py
+++ main.py
@@ -21,62 +21,54 @@ def get_db():  # важно сделать отдельное соединени
         con.close()
 
 
-# Предполагается, что период принятия таблеток поступает в виде временных меток, разделённых пробелом. То есть, к примеру: 8:00 9:20 10:30
-# Затем разделяем по пробелам, округляем, если нужно, и добавляем в таблицу
-def parse_period(period: str):
-    split_period = []
-
-    for stamp in period.split():
-        hour_str, minute_str = stamp.split(':')
-        hour, minute = int(hour_str), int(minute_str)
-
-        if hour < 8 or (hour >= 22 and minute > 0):
-            raise ValueError("Time period must be within 8:00-22:00")
-
-        if minute < 0 or minute > 59:
-            raise ValueError("Incorrect time format")
-
-        if minute % 15 == 0:  # если кратно 15 минутам, то просто добавляем как есть
-            split_period.append(stamp)
-
-        else:
-            minute += 15 - minute % 15  # округление до 15 минут
-
-            if minute == 60:
-                split_period.append(str(hour + 1) + ":00")  # добавили час
-
-            else:
-                split_period.append(hour_str + ':' + str(minute))
-
-    return split_period
-
-
 @app.post("/schedule")
 def add_schedule(data: dict, con: sqlite3.Connection = Depends(get_db)):
     cur = con.cursor()
 
     try:
-        pill, period_str, duration, user = data["pill_name"], data["time_period"], data["duration_days"], data["user_id"]
+        pill, period, duration, user = data["pill_name"], data["time_period"], data["duration_days"], data["user_id"]
 
-        period = parse_period(period_str)
+        period_rounded = []
+
+        for stamp in period:  # округление до 15 минут
+            hour_str, minute_str = stamp.split(':')
+            hour, minute = int(hour_str), int(minute_str)
+
+            if hour < 8 or (hour >= 22 and minute > 0):
+                raise ValueError("Time period must be within 8:00-22:00")
+
+            if minute < 0 or minute > 59:
+                raise ValueError("Incorrect time format")
+
+            if minute % 15 == 0:  # если кратно 15 минутам, то просто добавляем как есть
+                period_rounded.append(stamp)
+
+            else:
+                minute += 15 - minute % 15  # округление до 15 минут
+
+                if minute == 60:
+                    period_rounded.append(str(hour + 1) + ":00")  # добавили час
+
+                else:
+                    period_rounded.append(hour_str + ':' + str(minute))
 
         schedule_id = cur.execute(f"""
             INSERT INTO schedule(pill_name, duration_days, user_id) VALUES
-                ({pill}, {duration}, {user})
+                ('{pill}', {duration}, {user})
             RETURNING schedule_id
         """).fetchone()
 
         con.commit()
 
-        for stamp in period:
+        for stamp in period_rounded:
             cur.execute(f"""
                 INSERT INTO time_period VALUES
-                    ({schedule_id}, {stamp})
+                    ({schedule_id}, '{stamp}')
             """)
             con.commit()
 
     except Exception as e:
-        return {"message": e}
+        return {"message": str(e)}
 
     return {"message": "Success!", "schedule_id": schedule_id}
 
@@ -93,7 +85,7 @@ def get_schedules(user_id: int, con: sqlite3.Connection = Depends(get_db)):
         """).fetchall()
 
     except Exception as e:
-        return {"message": e}
+        return {"message": str(e)}
 
     return {"message": "Success!", "data": data}
 
@@ -117,7 +109,7 @@ def get_schedule(schedule_id: int, con: sqlite3.Connection = Depends(get_db)):
         """).fetchall()
 
     except Exception as e:
-        return {"message": e}
+        return {"message": str(e)}
 
     return {"message": "Success!", "pill": pill, "time_period": time_period}
 
@@ -148,6 +140,6 @@ def next_takings(user_id: int, con: sqlite3.Connection = Depends(get_db)):
                 next_pills.append((pill_name, stamp))
 
     except Exception as e:
-        return {"message": e}
+        return {"message": str(e)}
 
     return {"message": "Success!", "next_pills": next_pills}
